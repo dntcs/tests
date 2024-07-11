9e77228a8ebcff0c91d2a0bdad96bee072a9134b
dntc <youvoodu@mail.ru>
2024-07-11 16:50:32 +0500

    1

--- Abc.py
+++ Abc.py
@@ -1,11 +1,7 @@
 import os
-
 current_path = os.path.abspath(__file__)
-
 parent_path = os.path.join(current_path, '..')
-
 print(parent_path)
-
 def get_all_files(path):
     for i_name in os.listdir():
         new_path = os.path.join(path, i_name)
@@ -14,26 +10,7 @@ def get_all_files(path):
             get_all_files(new_path)
         else:
             print('-', i_name)
-
 get_all_files(parent_path)
 
 
-print('Hello')
-
-
-
-
-
-
-
-
-
-# def recurs(count):
-#     print(count)
-#     if count == 5:
-#         return
-#     recurs(count+1)
-
-#     print(count)
-
-# recurs(0)
+print('Hello') 
